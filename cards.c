7f606f5c723d96fb0fd167eaf0096228d426aa06
Dorcas <dorcasingasha@gmail.com>
2022-10-30 14:20:52 +0000

    Card counter

--- cards.c
+++ cards.c
@@ -11,13 +11,16 @@
 int main(void)
 {
 	char card_name[3];
-
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
+	int count = 0;
 	int val = 0;
 
-	switch (card_name[0])
+	while (card_name[0] != 'X')
 	{
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+
+		switch (card_name[0])
+		{
 		case 'K':
 		case 'Q':
 		case 'J':
@@ -26,16 +29,25 @@ int main(void)
 		case 'A':
 			val = 11;
 			break;
+		case 'X':
+			continue;
 		default:
 			val = atoi(card_name);
+			if ((val < 1) || (val > 10))
+				{
+					puts("I don't understand that value!");
+					continue;
+				}
+		}
+
+		printf("The card value is: %i\n", val);
+
+		if ((val >= 3) && (val <= 6))
+			count++;
+		else if (val == 10)
+			count--;
+
+		printf("Current count: %i\n", count);
 	}
-
-	printf("The card value is: %i\n", val);
-
-	if ((val >= 3) && (val <= 6))
-		puts("Count has gone up");
-	else if (val == 10)
-		puts("Count has gone down");
-
 	return (0);
 }
